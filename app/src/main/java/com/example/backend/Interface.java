a28a4d6d28bc4def3cd012a490f690ac5176c554
renan-munhoz <101846544+renan-munhoz@users.noreply.github.com>
2024-11-24 21:33:56 -0300

    backend
    criacao backend

---
+++ app/src/main/java/com/example/backend/Interface.java
@@ -0,0 +1,18 @@
+package com.example.backend;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.POST;
+import retrofit2.http.Body;
+
+public interface Interface {
+    @GET("/getData")
+    Call<Filme> getData();
+
+    @GET("/identified/getData/{id}")
+    Call<Filme> getDataById(@Path("id") int id);
+
+    @POST("/identified/saveData")
+    Call<Filme> postData(@Body Filme data);
+}
